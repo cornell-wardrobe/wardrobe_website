931aff31400378ef91cf221ebf6c6296ef4a656f
Connor Chen <connorjchen2784@gmail.com>
2023-10-19 15:01:24 -0400

    changed from fix 200px to flex basis

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -5,7 +5,7 @@ import { faInstagram, faFacebookF } from "@fortawesome/free-brands-svg-icons";
 
 function SocialMediaComponent({ icon, platform, text }) {
   return (
-    <div className="text-center w-[200px]">
+    <div className="mt-4 text-center md:basis-1/3">
       <FontAwesomeIcon className="text-5xl" icon={icon} />
       <p className="font-bold mb-4">{platform}</p>
       <p className="text-sm font-bold text-gray-500">{text}</p>
@@ -17,10 +17,10 @@ function Footer() {
   return (
     <div className="py-24 bg-gray-50">
       <p className="text-4xl font-bold text-center mb-4">WARDROBE</p>
-      <p className="text-center font-bold text-sm mb-16 text-gray-500">
+      <p className="text-center font-bold text-sm mb-12 text-gray-500">
         Helping any student pursue any opportunity
       </p>
-      <div className="md:flex justify-around">
+      <div className="md:flex md:justify-around md:flex-row">
         <SocialMediaComponent
           icon={faEnvelope}
           platform="EMAIL"
