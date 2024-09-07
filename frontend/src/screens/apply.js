1531a7d36b1d6b288f01a57ee8d2bd48364c490a
cornell-wardrobe <149619414+cornell-wardrobe@users.noreply.github.com>
2024-09-07 15:07:30 -0400

    Update roles and application status (#34)
    * package-lock
    
    * Update to The Wardrobe at Cornell & remove FB footer
    
    * build title & footer changes
    
    * removed unused FB variabe
    
    * updated team roles & application status
    
    ---------
    
    Co-authored-by: Jenny  Chen <jennychen@dhcp-vl2041-14109.eduroam.cornell.edu>
    Co-authored-by: Jenny  Chen <jennychen@jennys-mbp.home>

--- frontend/src/screens/apply.js
+++ frontend/src/screens/apply.js
@@ -8,14 +8,18 @@ function Apply() {
         <div className="flex w-screen bg-slate-100 items-center h-full md:w-[50vw]">
           <div className="mx-4 mt-16 xs:mx-0 xs:mt-0 justify-center w-full md:ml-7">
             <p className="text-5xl pb-2 font-bold text-center md:text-left md:text-6xl">Nice to meet you!</p>
-            <p className="text-2xl pb-2 mr-4 text-center md:text-left">Applications are now closed. They will reopen for FA24 early next semester, hope to see you then!</p>
-            {/* <div className="flex flex-col justify-center items-center md:justify-normal md:items-start">
-              <Link to="https://docs.google.com/forms/d/e/1FAIpQLSc4XcGcU-1sL9fe4zseSa0Z1cVK7ME2BV7fNwJqP_BIv77rEQ/viewform" target="blank">
+            <p className="text-2xl pb-2 mr-4 text-center md:text-left">Applications are now opened for F24. We're excited to see you apply!</p>
+            <div className="flex flex-col justify-center items-center md:justify-normal md:items-start">
+            <a
+                href="https://docs.google.com/forms/d/1Uc2fvgIQuLscxT1cA9MHq6uPjJV5wJheH7uyNU6l8mo/viewform?edit_requested=true"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button type="button" className="bg-blue-700 text-white px-5 py-2">
                   <p className="text-white text-sm my-4 mx-6">APPLY</p>
                 </button>
-              </Link>
-            </div> */}
+              </a>
+            </div>
           </div>
         </div>
         <div className="flex items-center justify-center bg-slate-100">
